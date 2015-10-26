6cbc1fbbc2b4aa991684a073151dac42a1f42048
crystalfire7 <12specialreindeers@gmail.com>
2015-10-25 19:36:02 -0500

    Login Screen UI Improvements and Functionality

--- src/ui/PasswordProtector.java
+++ src/ui/PasswordProtector.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.xml.bind.annotation.XmlElement.DEFAULT;
+
 import ui.Tools;
 import javafx.animation.Animation.Status;
 import javafx.animation.Timeline;
@@ -17,6 +19,7 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.StringProperty;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.effect.DropShadow;
@@ -35,6 +38,7 @@ import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
@@ -51,7 +55,7 @@ public class PasswordProtector extends Application {
 		launch(args);
 	}
 	
-	private static final String pseudoPass = "henry";
+	private static final String pseudoPass = "password";
 	
 	private static final Image screw = Tools.createImage("screw.png");
 	private static final Image textIn = Tools.createImage("OverOn.png");
@@ -72,6 +76,8 @@ public class PasswordProtector extends Application {
 	private PasswordField passwordField;
 	private ImageView button;
 	private Text info;
+	private Text help;
+	//private Stage helpScreen;
 	
 
 
@@ -93,13 +99,33 @@ public class PasswordProtector extends Application {
 		primaryStage.setScene(trans);
 		primaryStage.show();
 		primaryStage.setOpacity(0.0);
-		
+		Stage back = initBack();
+		back.initOwner(primaryStage);
+		back.show();
+		back.centerOnScreen();
 		Stage s = initIntro();
-		s.initOwner(primaryStage);
+		s.setY(back.getY()+10);
+		s.setX(back.getX()+10);
+		s.initOwner(back);
 		s.show();
+		
 	}
 
+	private Stage initBack(){
+		Stage primaryStage = new Stage();
+		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		
+		AnchorPane root = new AnchorPane();
+		Scene scene = new Scene(root, 410 * wRatio, 170 * hRatio);
+		
+		BackgroundImage back2 = new BackgroundImage(shinyBack, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
+				new BackgroundSize(scene.getWidth(), scene.getHeight(), false, false, false, false));
+		root.setBackground(new Background(back2));
+		root.setEffect(new InnerShadow(3.0, Color.BLACK));
+		primaryStage.setScene(scene);
 
+		return primaryStage;
+	}
 	private Stage initIntro() {
 		
 		Stage primaryStage = new Stage();
@@ -111,7 +137,7 @@ public class PasswordProtector extends Application {
 		InnerShadow smallShade = new InnerShadow(2.0, Color.BLACK);
 		InnerShadow largeShade = new InnerShadow(5.0, Color.BLACK);
 		DropShadow out = new DropShadow(2.0, Color.BLACK);
-		DropShadow out2 = new DropShadow(3.0, Color.rgb(40,100,120));
+		DropShadow out2 = new DropShadow(10.0, Color.BLACK);
 		
 		ImageView screw1 = Tools.createImageView(screw, 15, 15, 3, 0.0, sRatio, wRatio, hRatio, out);
 		ImageView screw2 = Tools.createImageView(screw, 15, 15, 385, 0.0, sRatio, wRatio, hRatio, out);
@@ -121,11 +147,11 @@ public class PasswordProtector extends Application {
 		passwordField = new PasswordField();
 		
 		passwordField.setLayoutX(60*wRatio);
-		passwordField.setLayoutY(110*hRatio);
+		passwordField.setLayoutY(100*hRatio);
 		passwordField.setPrefSize(200*wRatio, 15*hRatio);
 		passwordField.setFont(Tools.createBoldFont(12, sRatio));
 
-		button = Tools.createImageView(textNone, 60, 60, 280, 85, sRatio, wRatio, hRatio, out);
+		button = Tools.createImageView(textNone, 60, 60, 280, 75, sRatio, wRatio, hRatio, out);
 		button.setOnMousePressed(e->
 		{
 			if(passwordField.getText().isEmpty())
@@ -144,15 +170,22 @@ public class PasswordProtector extends Application {
 			button.setImage(textIn);
 			process();
 		});
-		
+		Circle c = new Circle(40*wRatio,110*hRatio,10*sRatio, Color.rgb(230,240,240));
+		c.setEffect(largeShade);
+		Text q = Tools.createText(34, 100, wRatio, hRatio, "?", Color.rgb(80,215,240), smallShade, Tools.createBoldFont(18, sRatio));
+		//genHelp(primaryStage);
+		q.setOnMousePressed(e->help());
+		help= Tools.createText(60, 127, wRatio, hRatio, "Default Password: password.\nChange Login password in \"Settings\".", Color.rgb(220,240,250), smallShade, Tools.createBoldFont(10, sRatio));
+		help.setVisible(false);
 		Text name = Tools.createText(50, 15, wRatio, hRatio, "Password Protector",Color.rgb(80,215,240), smallShade, Tools.createBoldFont(26, sRatio));
-		Text instruction = Tools.createText(50, 85, wRatio, hRatio, "Enter Your Password:", Color.rgb(80,215,240), smallShade, Tools.createRegularFont(18, sRatio));
-		info = Tools.createText(25, 55.0, wRatio, hRatio, "One Password to Save Them All! ", Color.rgb(200,220,230), null, Tools.createRegularFont(13, sRatio));
+		Text instruction = Tools.createText(50, 75, wRatio, hRatio, "Enter Your Password:", Color.rgb(80,215,240), smallShade, Tools.createRegularFont(18, sRatio));
+		
+		info = Tools.createText(25, 55.0, wRatio, hRatio, "One Password to Save them ALL!", Color.rgb(200,220,230), null, Tools.createRegularFont(10, sRatio));
 		info.setWrappingWidth(350*wRatio);
 		info.setTextAlignment(TextAlignment.CENTER);
 		
 		BackgroundImage back = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
-				new BackgroundSize(scene.getWidth(), scene.getHeight(), false, false, false, false));
+				new BackgroundSize(background.getWidth()/3, background.getHeight()/3, false, false, false, false));
 		BackgroundImage back2 = new BackgroundImage(shinyBack, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
 				new BackgroundSize(scene.getWidth(), scene.getHeight(), false, false, false, false));
 		
@@ -194,9 +227,9 @@ public class PasswordProtector extends Application {
 		passwordField.setBackground(new Background(back2));
 		passwordField.setEffect(smallShade);
 		
-		Rectangle panel = Tools.createRoundedRectangle(350, 25, 5, 5, 25, 50, sRatio, wRatio, hRatio, Color.DARKGRAY.darker().darker(), largeShade);
+		Rectangle panel = Tools.createRoundedRectangle(350, 20, 5, 5, 25, 50, sRatio, wRatio, hRatio, Color.DARKGRAY.darker().darker(), largeShade);
 		
-		root.getChildren().addAll(screw1, screw2, screw3, screw4, panel,name, instruction,passwordField,button,info);
+		root.getChildren().addAll(screw1, screw2, screw3, screw4, panel,name, instruction,passwordField,button,info,c,q, help);
 		root.setBackground(new Background(back));
 		root.setEffect(largeShade);
 		primaryStage.setScene(scene);
@@ -204,6 +237,7 @@ public class PasswordProtector extends Application {
 		return primaryStage;
 
 	}
+	
 	public void process()
 	{
 		if(passwordField.getText().equals(pseudoPass))
@@ -218,4 +252,40 @@ public class PasswordProtector extends Application {
 		}
 		button.setImage(textNone);
 	}
+	public void help()
+	{
+		if(help.isVisible())
+		{
+			help.setVisible(false);
+			return;
+		}
+		help.setVisible(true);
+	}
+	/*
+	public void genHelp(Stage s)
+	{
+		helpScreen = new Stage();
+		helpScreen.initStyle(StageStyle.TRANSPARENT);
+		
+		AnchorPane root = new AnchorPane();
+		Scene scene = new Scene(root, 120 * wRatio, 60 * hRatio);
+		
+		BackgroundImage back = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
+				new BackgroundSize(background.getWidth()/3, background.getHeight()/3, false, false, false, false));
+		root.setBackground(new Background(back));
+		root.setEffect(new DropShadow(3.0, Color.BLACK));
+		helpScreen.setX(100*wRatio);
+		helpScreen.setY(175*hRatio);
+		helpScreen.setScene(scene);
+		helpScreen.initOwner(s);
+	}
+	public void help()
+	{
+		if(helpScreen.isShowing())
+		{
+			helpScreen.hide();
+			return;
+		}
+		helpScreen.show();
+	}*/
 }
